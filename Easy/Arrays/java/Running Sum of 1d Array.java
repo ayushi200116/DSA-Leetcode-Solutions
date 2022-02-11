fbae73bd9a2ca5319a947db95ca2736356b3e8bf
Ayushi Indu <singhayushi535@gmail.com>
2022-02-12 01:54:41 +0530

    Added a java solution for problem Running Sum of 1d Array

--- Easy/Arrays/java/Running Sum of 1d Array.java
+++ Easy/Arrays/java/Running Sum of 1d Array.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] runningSum = new int[nums.length];
+        int[] runningsum = new int[nums.length];
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum=sum+nums[i];
-            runningSum[i]=sum;
+            runningsum[i]=sum;
         }
-        return runningSum;
+        return runningsum;
     }
 }
